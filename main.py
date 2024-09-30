206ee21ce7742c78375e67e989f6c3822e2c1915
Joe Gran <132323475+jgransinger@users.noreply.github.com>
2024-09-30 18:31:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,51 @@
 import random
-from art import logo
+from game_data import data
+from art import logo, vs
 
-def calc_guess(guess):
-    if guess > number and 0 < guess < 100 and attempts > 0:
-        if attempts > 0:
-            calc_attempts()
-            guess = int(input("TOO HIGH! Guess again\n"))
-            (print(f"You have {attempts} guesses left. Be careful!"))
-            calc_guess(guess)
-    elif guess == number:
-        print("HOLY SMOKES, YOU GOT IT. YOU WIN!")
-    elif guess < number and 0 < guess < 100 and attempts > 0:
-        if attempts > 0:
-            calc_attempts()
-            guess = int(input("TOO LOW! Guess again\n"))
-            (print(f"You have {attempts} guesses left. Be careful!"))
-            calc_guess(guess)
+#place new person in a function and assign a variable to each
+def assign_new_people():
+        person1 = data[random.randint(0,len(data) - 1)]
+        person2 = data[random.randint(0,len(data) - 1)]
+        people = [person1, person2]
+        return people
+
+#compare the follower counts
+def compare_followers(people):
+    print(people[0])   #CHANGE TO SENTENCES WHEN WORKING
+    print(people[1])   #CHANGE TO SENTENCES WHEN WORKING
+    if people[0]['follower_count'] > people[1]['follower_count']:
+        higher_follower_count = 0
+        print(f"{people[0]['name']} has more followers")   #DELETE WHEN WORKING
+    elif people[1]['follower_count'] > people[0]['follower_count']:
+        higher_follower_count = 1
+        print(f"{people[1]['name']} has more followers")    #DELETE WHEN WORKING
     else:
-        print("Stay between 1 and 100 next time. Game Over. Please restart game.")
+        print("Something went wrong")    #DELETE WHEN WORKING
+        higher_follower_count = "Something went wrong"    #FIX FIX FIX
+    return higher_follower_count
 
-def calc_attempts():
-    global attempts
-    attempts -= 1
-    if attempts <= 0:
-        print("You Lose! No more attempts left. Sorry my dude.")
+#Player guesses who has more followers
+def game():
+    winner = compare_followers(assign_new_people())
+    choice = int(input("Guess who has a higher follower count. Type '0' or '1': "))
+    if choice == winner:
+        print("CORRECT.\n")
 
-#START OF GAME
+        play_again()
+        # Enter code to keep score / add a point
+        ## ENTER CODE TO KEEP THE GAME GOING / PERFORM PERSON SWAP
+    else:
+        print("WRONG. Game Over.")
+        ## ENTER CODE FOR GAME OVER. GIVE FINAL SCORE, play again, ETC.
 
-print(logo)
-print("I am thinking of a number between 1 and 100.")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': \n").lower()
-number = random.randint(1,100)
-attempts = 0
+#Person B become person A, person A becomes random then person B
+def new_person_swap(people):
+    people[0] = people[1]
+    people[1] = data[random.randint(0,len(data) - 1)]
+    return people
 
-if difficulty == 'easy':
-    attempts = 10
-elif difficulty == 'hard':
-    attempts = 5
-else:
-    print("Uhh. That was not it. Restart the game. ")
-print(f"You have {attempts} guesses remaining. Good luck!") #shows not defined in IDE but works due to no block scope in python
-print(f"The answer is: {number}") ## DELETE THIS AFTER TESTING
-calc_guess(int(input("What is your first guess?: \n")))
+def play_again():
+    game()
 
+## START GAME
+game()
